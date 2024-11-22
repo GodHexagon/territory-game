6650054d721a191c346c5a694417b436e9751ca8
高見亨 <god.hexagon.bifry@gmail.com>
2024-11-22 14:58:38 +0900

    相対参照に変更

--- src/view/__init__.py
+++ src/view/__init__.py
@@ -1,5 +1,5 @@
-from view.view import Displayable
-from view.board import BoardView
-from view.picker import PickerView
+from .view import Displayable
+from .board import BoardView
+from .picker import PickerView
 
 __all__ = ['Displayable', 'BoardView', 'PickerView']

a3e69cee802f84137d2fbe15790db90d8e56e622
高見亨 <god.hexagon.bifry@gmail.com>
2024-11-27 10:33:18 +0900

    BoardView関連をリファクタリング

--- src/basic/view/board.py
+++ src/basic/view/board.py
@@ -11,7 +11,14 @@ class BoardView(View, LimitableArea):
 
     def __init__(self, x, y, w, h, cursor: Cursor):
         super().__init__(x, y, w, h)
-        self.board = DraggableBoard(self.x + self.w // 2, self.y + self.h // 2, self.drawer)
+
+        self.board = DraggableBoard(
+            self.x + self.w // 2, 
+            self.y + self.h // 2, 
+            self.drawer, 
+            self.__draw_tiles( [[0 for _ in range(DraggableBoard.BOARD_SIZE_TILES)] for _ in range(DraggableBoard.BOARD_SIZE_TILES)] )
+        )
+
         self.b_input = self.board.ini_b_input()
         self.cursor = cursor
         self.dg: Dragging | None = None
@@ -36,7 +43,10 @@ class BoardView(View, LimitableArea):
 
         self.board.make_sizing_bi(self.b_input)
         self.b_input.update(self.cursor)
-
+    
+    def draw(self):
+        self.board.draw()
+        
     def __limited_move(self, to:Tuple[int, int]):
         """盤がビューの外に出ないようにしつつ盤を移動"""
         gap = self.DRAGGABLE_LIMIT_POCKET_PX
@@ -44,9 +54,38 @@ class BoardView(View, LimitableArea):
             min(self.x + self.w + self.board.w / 2 - gap, max(self.x - self.board.w / 2 + gap, to[0])),
             min(self.y + self.h + self.board.w / 2 - gap, max(self.y - self.board.h / 2 + gap, to[1]))
         )
-    
-    def draw(self):
-        self.board.draw()
+        
+    def __draw_tiles(self, data:List[List[int]]) -> pyxel.Image:
+        """タイルマップをもとに画像を生成し、これを返す"""
+        from pyxres import EMPTY_TILE_COOR, BLOCK_TILE_COOR, TILE_COLOR_PALLET_NUMBER, DEFAULT_COLOR_S, RED_COLOR_S, BLUE_COLOR_S
+
+        image = pyxel.Image(DraggableBoard.BOARD_SIZE_TILES * TILE_SIZE_PX, DraggableBoard.BOARD_SIZE_TILES * TILE_SIZE_PX)
+        x = 0
+        y = 0
+        for column in data:
+            y = 0
+            for t in column:
+                if t == 0: tile_coor = EMPTY_TILE_COOR
+                else:
+                    tile_coor = BLOCK_TILE_COOR
+                    if t == 1:
+                        for i in range(TILE_COLOR_PALLET_NUMBER): image.pal(i + DEFAULT_COLOR_S, i + RED_COLOR_S)
+                    elif t == 2:
+                        for i in range(TILE_COLOR_PALLET_NUMBER): image.pal(i + DEFAULT_COLOR_S, i + BLUE_COLOR_S)
+                image.blt(
+                    x,
+                    y,
+                    pyxel.images[0],
+                    tile_coor[0],
+                    tile_coor[1],
+                    TILE_SIZE_PX,
+                    TILE_SIZE_PX
+                )
+                image.pal()
+
+                y += TILE_SIZE_PX
+            x += TILE_SIZE_PX
+        return image
 
 class Dragging:
     """ドラッグが開始してから離すまでが寿命のクラス"""
@@ -57,10 +96,11 @@ class Dragging:
     def get_board_pos(self, curr_mouse_pos:Tuple[float, float]) -> Tuple[float, float]:
         return tuple(self.board_pos[i] + curr_mouse_pos[i] - self.mouse_pos[i] for i in range(2))
 
+from pyxres import TILE_SIZE_PX
+
 class DraggableBoard(CenteredArea):
     """移動可能な盤の座標系を表す"""
     FRAME_THICKNESS = 10
-    from pyxres import TILE_SIZE_PX
 
     BOARD_SIZE_TILES = 20
 
@@ -68,20 +108,23 @@ class DraggableBoard(CenteredArea):
     MIN_ZOOM = 1.0
     MAX_ZOOM = 25.0
 
-    def __init__(self, cx:float, cy:float, drawer:LimitedDrawer):
+    def __init__(self, cx:float, cy:float, drawer:LimitedDrawer, tiles: pyxel.Image):
         self.x = 0.0
         self.y = 0.0
         self.w = float(DraggableBoard.DEFAULT_BOARD_SIZE_PX)
         self.h = float(DraggableBoard.DEFAULT_BOARD_SIZE_PX)
         self.scale = 1.0
         self.drawer = drawer
-        self.tiles = self.__draw_tiles( [[0 for _ in range(self.BOARD_SIZE_TILES)] for _ in range(self.BOARD_SIZE_TILES)] )
+        self.tiles = tiles
         self.to_center_pos(cx, cy)
     
     def ini_b_input(self):
         F = DraggableBoard.FRAME_THICKNESS
-        T = DraggableBoard.BOARD_SIZE_TILES * DraggableBoard.TILE_SIZE_PX
+        T = DraggableBoard.BOARD_SIZE_TILES * TILE_SIZE_PX
         return BoardInput(self.x + F, self.y + F, T, T, self.scale)
+    
+    def set_tiles(self, value: pyxel.Image):
+        self.tiles = value
     
     def zoom(self, scale:float):
         """現在の大きさに対しての拡大倍率を指定。さらに、中心位置を保持。"""
@@ -100,11 +143,12 @@ class DraggableBoard(CenteredArea):
         return effected_scale
     
     def make_sizing_bi(self, bi: 'BoardInput'):
+        """BoardInputの形を、適切な形に調整。"""
         F = DraggableBoard.FRAME_THICKNESS
         bi.x = self.x + F * self.scale
         bi.y = self.y + F * self.scale
 
-        T = DraggableBoard.BOARD_SIZE_TILES * DraggableBoard.TILE_SIZE_PX
+        T = DraggableBoard.BOARD_SIZE_TILES * TILE_SIZE_PX
         bi.w = T * self.scale
         bi.h = T * self.scale
 
@@ -127,38 +171,6 @@ class DraggableBoard(CenteredArea):
             self.tiles.height,
             scale=self.scale
         )
-    
-    def __draw_tiles(self, data:List[List[int]]) -> pyxel.Image:
-        """タイルマップをもとに画像を生成し、これを返す"""
-        from pyxres import EMPTY_TILE_COOR, BLOCK_TILE_COOR, TILE_COLOR_PALLET_NUMBER, DEFAULT_COLOR_S, RED_COLOR_S, BLUE_COLOR_S
-
-        image = pyxel.Image(self.BOARD_SIZE_TILES * self.TILE_SIZE_PX, self.BOARD_SIZE_TILES * self.TILE_SIZE_PX)
-        x = 0
-        y = 0
-        for column in data:
-            y = 0
-            for t in column:
-                if t == 0: tile_coor = EMPTY_TILE_COOR
-                else:
-                    tile_coor = BLOCK_TILE_COOR
-                    if t == 1:
-                        for i in range(TILE_COLOR_PALLET_NUMBER): image.pal(i + DEFAULT_COLOR_S, i + RED_COLOR_S)
-                    elif t == 2:
-                        for i in range(TILE_COLOR_PALLET_NUMBER): image.pal(i + DEFAULT_COLOR_S, i + BLUE_COLOR_S)
-                image.blt(
-                    x,
-                    y,
-                    pyxel.images[0],
-                    tile_coor[0],
-                    tile_coor[1],
-                    self.TILE_SIZE_PX,
-                    self.TILE_SIZE_PX
-                )
-                image.pal()
-
-                y += self.TILE_SIZE_PX
-            x += self.TILE_SIZE_PX
-        return image
 
 class BoardInput(LimitableArea):
     def __init__(self, x, y, w, h, scale: float):
@@ -167,6 +179,7 @@ class BoardInput(LimitableArea):
         self.prev_hovered = False
     
     def set_shape(self, x, y, w, h, scale):
+        """形の調整を受けとる。"""
         self.x = x
         self.y = y
         self.w = w

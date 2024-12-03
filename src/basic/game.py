bd75b1bc860fcd025f1d194d5a05502c56965150
高見亨 <god.hexagon.bifry@gmail.com>
2024-12-03 16:20:02 +0900

    modified:   src/basic/game.py

--- src/basic/game.py
+++ src/basic/game.py
@@ -6,36 +6,30 @@ from .key_bind import *
 from pyxres import BLUE_COLOR_S
 from typing import Dict
 
-class GameView(Area, ParenthoodView):
+class GameView(Area, View):
     def __init__(self, x, y, w, h):
         super().__init__(x, y, w, h)
         
         self.game = RuleVSAI(self.on_change_pieces)
         self.rotation = Rotation.DEFAULT
         
-        new = Cursor()
+        self.cursor = Cursor()
 
         board_view_end_y = int(h * 0.6)
-        self.childs: Dict[View] = {
-            "b": BoardView(0, 0, w, board_view_end_y, new, BLUE_COLOR_S, self.game),
-            "p": PickerView(
-                0, 
-                board_view_end_y + 1, 
-                w, 
-                h - board_view_end_y - 1, 
-                self.game.get_pieces_shape(RuleVSAI.PLAYER),
-                BLUE_COLOR_S,
-                new
-            ),
-            'c': new
-        }
+        self.board = BoardView(0, 0, w, board_view_end_y, self.cursor, BLUE_COLOR_S, self.game)
+        self.picker = PickerView(
+            0, 
+            board_view_end_y + 1, 
+            w, 
+            h - board_view_end_y - 1, 
+            self.game.get_pieces_shape(RuleVSAI.PLAYER),
+            BLUE_COLOR_S,
+            self.cursor
+        )
         
-        picker: PickerView = self.childs['p']
-        picker.set_piece_rotation(self.rotation)
-        board: BoardView = self.childs['b']
-        board.set_piece_rotation(self.rotation)
-        cursor: Cursor = self.childs['c']
-        cursor.set_rotation(self.rotation)
+        self.picker.set_piece_rotation(self.rotation)
+        self.board.set_piece_rotation(self.rotation)
+        self.cursor.set_rotation(self.rotation)
     
     def on_change_pieces(self, player: int, data: GameData):
         assert False, 'ピースが置かれた！'
@@ -45,14 +39,15 @@ class GameView(Area, ParenthoodView):
             self.rotation = Rotation.counter_cw(self.rotation)
         if btnp(Bind.ROTATE_RIGHT):
             self.rotation = Rotation.cw(self.rotation)
-        picker: PickerView = self.childs['p']
-        picker.set_piece_rotation(self.rotation)
-        board: BoardView = self.childs['b']
-        board.set_piece_rotation(self.rotation)
-        cursor: Cursor = self.childs['c']
-        cursor.set_rotation(self.rotation)
+        self.picker.set_piece_rotation(self.rotation)
+        self.board.set_piece_rotation(self.rotation)
+        self.cursor.set_rotation(self.rotation)
 
-        return super().update()
+        self.picker.update()
+        self.board.update()
+        self.cursor.update()
     
     def draw(self):
-        return super().draw()
+        self.picker.draw()
+        self.board.draw()
+        self.cursor.draw()

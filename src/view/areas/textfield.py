9beffc261d7f9ffbe656802ddbb8ca395f34a4fd
高見亨 <god.hexagon.bifry@gmail.com>
2025-01-06 20:27:50 +0900

    テキストフィールドの色が正しい

--- src/view/areas/textfield.py
+++ src/view/areas/textfield.py
@@ -7,39 +7,39 @@ from typing import *
 import pyxel
 
 class TextField(Area):
-    HEIGHT_PX = 24
+    ITEM_HEIGHT_PX = 24
     MARGIN_PX = 8
     GAP_PX = 8
 
     def __init__(self, x, y, w, on_changed: Callable[[str], None], default: str = ""):
-        HEIGHT = TextField.HEIGHT_PX
+        HEIGHT = TextField.ITEM_HEIGHT_PX
         MARGIN = TextField.MARGIN_PX
 
-        super().init_area(x, y, w, HEIGHT + MARGIN * 2)
+        self.init_area(x, y, w, HEIGHT + MARGIN * 2)
 
         self.on_changed = on_changed
-
-        self.set_colors()
 
         TX, TY = RIGHT_ALLOW_ICON_COOR
         self.write_b = IconButton(
             self.x + MARGIN + HEIGHT / 2, self.y + self.h / 2, size=HEIGHT, on_click=self.__hdl_write,
             tile_selector=(TX, TY, TILE_SIZE_PX, TILE_SIZE_PX)
         )
-        self.write_b.set_colors(text=self.colors[0], backgroud=self.colors[1])
 
         self.field = TextButton(*self.get_center_pos(), lambda: None, default)
-        self.field.set_colors(text=self.colors[0], backgroud=COLOR_WHITE, border=COLOR_WHITE)
         
         TX, TY = COPY_ICON_COOR
         self.copy_b = IconButton(
             self.x + self.w - MARGIN - HEIGHT / 2, self.y + self.h / 2, size=HEIGHT, on_click=self.__hdl_copy,
             tile_selector=(TX, TY, TILE_SIZE_PX, TILE_SIZE_PX)
         )
-        self.copy_b.set_colors(text=self.colors[0], backgroud=self.colors[1])
+
+        self.set_colors()
     
     def set_colors(self, fill: int = COLOR_BLACK, background: int = COLOR_PRIMARY):
-        self.colors = (fill, background)
+        self.background = background
+        self.write_b.set_colors(text=fill, backgroud=background, border=background)
+        self.field.set_colors(text=fill, backgroud=COLOR_WHITE, border=COLOR_WHITE)
+        self.copy_b.set_colors(text=fill, backgroud=background, border=background)
     
     def __hdl_write(self):
         self.on_changed("test")
@@ -52,7 +52,7 @@ class TextField(Area):
         self.copy_b.update()
     
     def draw(self):
-        pyxel.rect(self.x, self.y, self.w, self.h, self.colors[1])
+        pyxel.rect(self.x, self.y, self.w, self.h, self.background)
         self.write_b.draw()
         self.field.draw()
         self.copy_b.draw()

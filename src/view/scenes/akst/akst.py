fcc41b27802c80acb65bd77393a0fb54f9d24a31
高見亨 <god.hexagon.bifry@gmail.com>
2025-01-07 18:11:37 +0900

    アクセスキーマネージャはエラーをユーザに伝えられる

--- src/view/scenes/akst/akst.py
+++ src/view/scenes/akst/akst.py
@@ -2,6 +2,8 @@ from ...view import View, Area
 from src.pyxres import *
 from ...areas.text import WritenText
 from ...areas.button import TextButton
+from ...areas.textfield import TextField
+from ...sequencer.access_key import AccessKeyManager
 
 import pyxel
 
@@ -24,13 +26,15 @@ class Window(Area):
     MARGIN_PX = 16
 
     def __init__(self, cx: float, cy: float, on_complete: Callable[[bool], None]) -> None:
-        super().init_area(0, 0, *Window.SIZE_PX)
+        self.init_unplaced_area(*Window.SIZE_PX)
         self.to_center_pos(cx, cy)
 
         MARGIN = Window.MARGIN_PX
 
         self.title = WritenText(cx, 0, text="SET YOUR ACCESS KEY", color=COLOR_BLACK, scale=5)
         self.title.to_y(self.y + MARGIN)
+
+        self.console: WritenText | TextField = WritenText(cx, cy, "PROCESSING...", COLOR_FAILURE)
 
         self.update_b = TextButton(0, 0, lambda : on_complete(True), "UPDATE")
         self.update_b.set_colors(backgroud=COLOR_SUCCESSFULL)
@@ -40,13 +44,33 @@ class Window(Area):
         self.cancel_b = TextButton(0, 0, lambda : on_complete(False), "CANCEL")
         self.cancel_b.to_x(self.x + MARGIN)
         self.cancel_b.to_y_bottom(self.y + self.h - MARGIN)
+        
+        self.akm = AccessKeyManager(self.__hdl_throw_error, self.__hdl_load_current_sk)
+        ok = self.akm.load()
+        if not ok: raise AssertionError("Oh no")
+    
+    def __hdl_throw_error(self):
+        self.console = WritenText(*self.get_center_pos(), "ERROR", COLOR_FAILURE)
+    
+    def __hdl_change_ak(self):
+        pass
+
+    def __hdl_load_current_sk(self, value: str):
+        MARGIN = Window.MARGIN_PX
+        self.console = TextField(lambda s: None, value)
+        self.console.set_w(self.w - MARGIN * 2)
+        self.console.to_center_pos(*self.get_center_pos())
     
     def update(self):
+        if isinstance(self.console, TextField):
+            self.console.update()
         self.update_b.update()
         self.cancel_b.update()
+        self.akm.update()
     
     def draw(self):
         pyxel.rect(self.x, self.y, self.w, self.h, COLOR_PRIMARY)
         self.title.draw()
+        self.console.draw()
         self.update_b.draw()
         self.cancel_b.draw()
---
+++ src/view/sequencer/access_key.py
@@ -0,0 +1,114 @@
+from ..view import View
+
+from pathlib import Path
+import threading
+
+from typing import *
+from enum import Enum
+import os
+import platform
+
+class AccessKeyManager(View):
+    def __init__(self,
+        on_error_throwed: Callable[[], None],
+        on_load_complete: Callable[[str], None]
+    ):
+        self.on_error_throwed = on_error_throwed
+        self.on_load_complete = on_load_complete
+
+        self.manager = Access()
+
+        self._common_lock = threading.Lock()
+        self._ak: str | None = None
+        self._errored = False
+    
+    def load(self):
+        def hdl_read_ak():
+            result = self.manager.read()
+
+            with self._common_lock:
+                if isinstance(result, int):
+                    self._errored = True
+                else:
+                    self._ak = result if result is not None else ""
+        
+        if self._common_lock.acquire(blocking=False):
+            trd = threading.Thread(target=hdl_read_ak)
+            trd.start()
+
+            self._common_lock.release()
+
+            processed = True
+        else: 
+            processed = False 
+        
+        return processed
+
+    def update(self):
+        if self._common_lock.acquire(blocking=False):
+            if self._errored:
+                self.on_error_throwed()
+                self._errored = False
+            if self._ak is not None:
+                self.on_load_complete(self._ak)
+                self._ak = None
+
+            self._common_lock.release()
+    
+    def draw(self):
+        pass
+
+class Access:
+    ROOT_PATH = "territory_game"
+    LOG_FILE_PATH = "territory_game/log.txt"
+    SECRET_DIRECTORY_PATH = "territory_game/secret"
+    SECRET_FILE_PATH = "territory_game/secret/secret.properties"
+    PROPERTY_NAME = "ACCESS_KEY"
+
+    def write(self, key: str):
+        # ホームディレクトリのパスを取得
+        home = Path.home()
+
+        # 新しいフォルダを作成
+        new_folder = home / self.SECRET_DIRECTORY_PATH
+
+        # フォルダが存在しない場合は作成（parents=Trueで親ディレクトリも作成）
+        new_folder.mkdir(parents=True, exist_ok=True)
+
+        # フォルダ内にファイルを保存
+        file_path = home / self.SECRET_FILE_PATH
+        file_path.write_text(f'{self.PROPERTY_NAME}={key}', encoding='utf-8')
+
+    def read(self) -> str | None | int:
+        # ホームディレクトリからのパスを作成
+        home = Path.home()
+        directory = home / self.SECRET_DIRECTORY_PATH
+        directory.mkdir(parents=True, exist_ok=True)
+        file_path = home / self.SECRET_FILE_PATH
+
+        try:
+            if not file_path.exists():
+                return None
+            # ファイルを読み込む
+            content = file_path.read_text(encoding='utf-8')
+            return content
+            
+        except OSError as e:
+            self.__log("OSとの連携に失敗しました。プログラムとディレクトリの権限が影響している可能性があります。")
+            return -1
+    
+    def __log(self, message: str):
+        home = Path.home()
+        directory = home / self.SECRET_DIRECTORY_PATH
+        directory.mkdir(parents=True, exist_ok=True)
+        log_path = home / self.LOG_FILE_PATH
+
+        log_path.write_text(f'エラー：{message}', encoding='utf-8')
+
+        # OSによって異なるコマンドを使用してファイルを開く
+        if platform.system() == 'Windows':
+            os.startfile(log_path)
+        elif platform.system() == 'Darwin':  # macOS
+            os.system(f'open {log_path}')
+        else:  # Linux
+            os.system(f'xdg-open {log_path}')
